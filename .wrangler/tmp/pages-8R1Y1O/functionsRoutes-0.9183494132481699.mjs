990c77558e866bfe3e8cdcc11828100233b45ce6
Simon Shain <sashain6@gmail.com>
2026-04-03 07:49:52 -0700

    update

--- .wrangler/tmp/pages-8R1Y1O/functionsRoutes-0.9183494132481699.mjs
+++ .wrangler/tmp/pages-8R1Y1O/functionsRoutes-0.9183494132481699.mjs
@@ -2,8 +2,6 @@ import { onRequestPost as __api_auth_login_js_onRequestPost } from "C:\\Projects
 import { onRequestGet as __api_auth_me_js_onRequestGet } from "C:\\Projects\\Housing Editor\\functions\\api\\auth\\me.js"
 import { onRequestPost as __api_auth_register_js_onRequestPost } from "C:\\Projects\\Housing Editor\\functions\\api\\auth\\register.js"
 import { onRequestGet as __api_health_js_onRequestGet } from "C:\\Projects\\Housing Editor\\functions\\api\\health.js"
-import { onRequest as __api_test_js_onRequest } from "C:\\Projects\\Housing Editor\\functions\\api\\test.js"
-import { onRequest as __api_index_js_onRequest } from "C:\\Projects\\Housing Editor\\functions\\api\\index.js"
 
 export const routes = [
     {
@@ -33,19 +31,5 @@ export const routes = [
       method: "GET",
       middlewares: [],
       modules: [__api_health_js_onRequestGet],
-    },
-  {
-      routePath: "/api/test",
-      mountPath: "/api",
-      method: "",
-      middlewares: [],
-      modules: [__api_test_js_onRequest],
-    },
-  {
-      routePath: "/api",
-      mountPath: "/api",
-      method: "",
-      middlewares: [],
-      modules: [__api_index_js_onRequest],
     },
   ]
--- functions/api/test.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(request, env, ctx) {
-  return new Response(JSON.stringify({ message: 'Functions are working!' }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
